1cbe5de931e9b28c176717b90714535e6bde8d71
Soumya <sswaroop@thoughtworks.com>
2017-07-13 16:14:05 +0530

    installing latest gauge

--- install_latest_gauge.sh
+++ install_latest_gauge.sh
@@ -1,17 +1,19 @@
-GAUGE_LATEST_VERSION="0.9.0.nightly-2017-07-12"
+GAUGE_LATEST=`curl -w "%{url_effective}\n" -L -s -S https://github.com/getgauge/gauge/releases/latest -o /dev/null`
+
+GAUGE_LATEST_VERSION=`echo $GAUGE_LATEST | sed 's/.*\/v//'`
 
 BIT=`uname -m`
 
 if [ "$BIT"=="x86_64" ];
 then
-GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86_64.zip"
+	GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86_64.zip"
 else
-GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86.zip"
+	GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86.zip"
 fi
 
-GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/Gauge/download_file?file_path=linux%2F$GAUGE_FILE_NAME"
+GAUGE_DOWNLOAD_URL="https://github.com/getgauge/gauge/releases/download/v$GAUGE_LATEST_VERSION/$GAUGE_FILE_NAME"
 
-wget -O $GAUGE_FILE_NAME $GAUGE_DOWNLOAD_URL
+wget $GAUGE_DOWNLOAD_URL
 
 OUTPUT_DIR="./gauge_$GAUGE_LATEST_VERSION"
 
