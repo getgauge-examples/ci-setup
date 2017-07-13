fffc00433f5c6a21dfa128e38da051798b96236b
Soumya <sswaroop@thoughtworks.com>
2017-07-13 15:43:07 +0530

    correcting the nightly version name

--- install_latest_gauge.sh
+++ install_latest_gauge.sh
@@ -4,7 +4,7 @@ wget $GAUGE_DOWNLOAD_URL
 
 OUTPUT_DIR="./gauge_0.9.0.nightly-2017-07-12-linux.x86_64"
 
-unzip "gauge_0.9.0.nightly-2017-07-12-linux.x86_64.zip" -d $OUTPUT_DIR
+unzip "gauge-0.9.0.nightly-2017-07-12-linux.x86_64.zip" -d $OUTPUT_DIR
 
 cd $OUTPUT_DIR
 
