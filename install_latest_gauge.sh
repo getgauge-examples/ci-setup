47eef05bce3db9c3ee056a6a7c6d431141c433e2
Soumya <sswaroop@thoughtworks.com>
2017-07-13 15:52:17 +0530

    correcting the nightly version name

--- install_latest_gauge.sh
+++ install_latest_gauge.sh
@@ -1,10 +1,21 @@
-GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/Gauge/Nightly/0.9.0.nightly-2017-07-12/gauge-0.9.0.nightly-2017-07-12-linux.x86_64.zip"
+GAUGE_LATEST_VERSION="0.9.0.nightly-2017-07-12"
+
+BIT=`uname -m`
+
+if [ "$BIT"=="x86_64" ];
+then
+GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86_64.zip"
+else
+GAUGE_FILE_NAME="gauge-$GAUGE_LATEST_VERSION-linux.x86.zip"
+fi
+
+GAUGE_DOWNLOAD_URL="https://bintray.com/gauge/Gauge/download_file?file_path=linux%2F$GAUGE_FILE_NAME"
 
 wget $GAUGE_DOWNLOAD_URL
 
-OUTPUT_DIR="./gauge_0.9.0.nightly-2017-07-12-linux.x86_64"
+OUTPUT_DIR="./gauge_$GAUGE_LATEST_VERSION"
 
-unzip "gauge-0.9.0.nightly-2017-07-12-linux.x86_64.zip" -d $OUTPUT_DIR
+unzip $GAUGE_FILE_NAME -d $OUTPUT_DIR
 
 cd $OUTPUT_DIR
 
